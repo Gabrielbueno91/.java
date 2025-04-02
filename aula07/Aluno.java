e0d890dec727e8ed837ecffc2dfed56cb1d8efbb
Gabrielbueno91 <gbf2401@gmail.com>
2025-04-02 23:45:17 +0000

    aula07

---
+++ aula07/Aluno.java
@@ -0,0 +1,25 @@
+//Nome de classe sempre letra maiúscula
+public class Aluno{
+    //Atributos
+    String nome;
+    double nota1;
+    double nota2;
+
+    //construtor
+    public Aluno(String nome){
+        this.nome = nome;
+    }
+    
+    public Aluno(String nome, double nota1, double nota2){
+        this.nome = nome;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    // métodos
+    public double calculaMedia(){
+        double soma = nota1 + nota2;
+        return soma / 2;
+    }
+
+}
